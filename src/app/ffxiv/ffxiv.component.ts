f644d3e85aa89512f8409cf7879a0ce189fc9c56
DUBUS Benoit <benoit.dubus@ext.soprasteria.com>
2019-02-12 17:33:25 +0100

    FFXIV chr modification, traitement du JSON Ok

--- src/app/ffxiv/ffxiv.component.ts
+++ src/app/ffxiv/ffxiv.component.ts
@@ -10,7 +10,10 @@ export class FfxivComponent implements OnInit {
 
   Idlist: any[] = [11271710, 13376194, 734000];
   persoInfos: any[] = [];
+  bodyInfos: any[] = [];
   apiXivKey = '88fdf3601b1b4e9ebe38beaa';
+  xpBar: number;
+  bodyId: any;
 
   constructor(
     private httpClient: HttpClient
@@ -18,14 +21,17 @@ export class FfxivComponent implements OnInit {
 
   ngOnInit() {
     this.httpClient.get<any[]>(
-    'https://xivapi.com/character/734000?key=' + this.apiXivKey
-      ).subscribe(
+      'https://xivapi.com/character/' + this.Idlist[1] + '?key=' + this.apiXivKey
+    ).subscribe(
       (data) => {
-        this.persoInfos = data;
-        console.log(data);
-      }
-    );
+        this.persoInfos = data.Character;
+        this.httpClient.get<any[]>(
+          'https://xivapi.com/item/' + data.Character.GearSet.Gear.Body.ID + '?key=' + this.apiXivKey
+        ).subscribe(
+          (data) => {
+            this.bodyInfos = data;
+          });
+      });
+
   }
-
-
 }
